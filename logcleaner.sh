75704196f55c4e4323d2e16d659817c81a77204f
Ibad Usmani <58698637+ibadusmani@users.noreply.github.com>
2020-03-29 04:02:23 +0530

    Add files via upload

---
+++ logcleaner.sh
@@ -0,0 +1,8 @@
+# cleans the entries with no ip addresses in the line, cats lines with "ipv4|ipv6" moves them to temp and moves it back to the original file
+
+cat /home/username/logs/filename | grep -E "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b|\b([0-9A-Fa-f]{1,4}:){7}[0-9A-Fa-f]{1,4}\b" > temp && mv temp /home/username/logs/filename 
+
+# cleans entries made by web crawlers e.t.c 
+
+grep -v "http\|flare\|Flare\|bot\|perl\|python\|cloud\|Cloud\|face\|Face\|google" /home/username/logs/filename > temp && mv temp /home/username/logs/filename
+
